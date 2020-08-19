451e00ceb1b2969f41286c69d2c7bc864066b9f0
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-08-20 00:52:46 +0900

    :bug: Make detail page
    detail

--- frontend/src/Routes/Detail.tsx
+++ frontend/src/Routes/Detail.tsx
@@ -3,6 +3,36 @@ import axios from "axios";
 import Markdown from "../Components/Markdown";
 import { RouteComponentProps } from "react-router-dom";
 import { IData, IMatch } from "../Interfaces";
+import Header from "../Components/Header";
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 3em;
+  @media (max-width: 768px) {
+    padding: 2em;
+  }
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  width: 100%;
+  max-width: 1280px;
+`;
+
+const Title = styled.div`
+  margin-bottom: 0.3em;
+  font-size: 2.5em;
+  font-weight: 600;
+`;
+
+const Time = styled.div`
+  font-size: 1em;
+  opacity: 0.5;
+  font-weight: 600;
+  margin-bottom: 2em;
+`;
 
 const Detail: React.FC<RouteComponentProps<IMatch>> = ({ match }) => {
   const id = match.params.id;
@@ -17,15 +47,19 @@ const Detail: React.FC<RouteComponentProps<IMatch>> = ({ match }) => {
   console.log(data);
   return (
     <>
+      <Header />
       {data === null ? (
         <div>Loading ... </div>
       ) : (
-        <div>
-          <div key={data.id}>
-            <div>Title: {data.title}</div>
+        <Container>
+          <Wrapper key={data.id}>
+            <Title>{data.title}</Title>
+            <Time>
+              {data.created_at.slice(0, 10)} {data.created_at.slice(11, 19)}
+            </Time>
             <Markdown source={data.content} />
-          </div>
-        </div>
+          </Wrapper>
+        </Container>
       )}
     </>
   );
