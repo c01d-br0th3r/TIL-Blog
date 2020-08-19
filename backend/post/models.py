d36f284a285756d287993923f010b8a5bfa5174b
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-08-19 23:25:15 +0900

    :fire: Add tags field
    tags

--- backend/post/models.py
+++ backend/post/models.py
@@ -4,6 +4,8 @@ class Post(models.Model):
 	title = models.CharField(max_length=200)
 	summary = models.TextField(null=True, default='')
 	content = models.TextField()
+	thumbnail = models.TextField()
+	tags = models.TextField()
 
 	def __str__(self):
 		"""A string representation of the model."""
