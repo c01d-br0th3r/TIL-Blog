e6a7124fb646b3778e36c995622a02e1281e03b9
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-08-19 23:31:23 +0900

    :wrench: Update typescript interface
    interface

---
+++ frontend/src/Interfaces/index.tsx
@@ -0,0 +1,12 @@
+export interface IData {
+  id: number;
+  title: string;
+  content: string;
+  summary: string;
+  thumbnail: string;
+  tags: string;
+}
+
+export interface IMatch {
+  id: string;
+}
